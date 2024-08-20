a0a4ccfca782fcddb03c370cfc796f7c7e40dc38
lmaltos <136147644+lmaltos@users.noreply.github.com>
2024-08-20 14:08:44 +0000

    QuickSort con template

--- Sorting/main.cpp
+++ Sorting/main.cpp
@@ -4,14 +4,26 @@ void insertionSort(int arr[], int n);
 
 using namespace std;
 int main() {
-    int my_arreglo[8] = {15,22,13,27,12,10,20,25};
+    int my_arreglo_int[8] = {15,22,13,27,12,10,20,25};
+    string my_arreglo[8] = {"hola", "mundo", "quick", "sort", "merge", "bubble", "intercambio", "insercion"};
+    cout << "my_arreglo_int [ ";
+    for (int i = 0; i < 8; i++) {
+        cout << my_arreglo_int[i] << " ";
+    }
+    cout << "]" << endl;
     cout << "my_arreglo [ ";
     for (int i = 0; i < 8; i++) {
         cout << my_arreglo[i] << " ";
     }
     cout << "]" << endl;
     //insertionSort(my_arreglo,10);
-    quickSort(my_arreglo,0,8);
+    quickSort<int>(my_arreglo_int,0,8);
+    quickSort<string>(my_arreglo,0,8);
+    cout << "my_arreglo_int [ ";
+    for (int i = 0; i < 8; i++) {
+        cout << my_arreglo_int[i] << " ";
+    }
+    cout << "]" << endl;
     cout << "my_arreglo [ ";
     for (int i = 0; i < 8; i++) {
         cout << my_arreglo[i] << " ";
--- Sorting/quickSort.h
+++ Sorting/quickSort.h
@@ -1,6 +1,35 @@
 #ifndef QUICKSORT_H
 #define QUICKSORT_H
 
-void quickSort(int arr[], int inicio, int fin);
+template <typename T>
+void quickSort(T arr[], int inicio, int fin);
 
+template <typename T>
+int particion(T arr[],int ini,int fin);
+
+template <typename T>
+void quickSort(T arr[], int ini, int fin) {
+    if (ini < fin) {
+        int pivote = particion(arr,ini,fin);
+        quickSort(arr,ini,pivote);
+        quickSort(arr,pivote + 1,fin);
+    }
+}
+
+template <typename T>
+int particion(T arr[],int ini,int fin) {
+    T pivote = arr[ini];
+    int i = ini;
+    for (int j = ini + 1; j < fin; j++) {
+        if (arr[j] < pivote) {
+            i++;
+            T tmp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = tmp;
+        }
+    }
+    arr[ini] = arr[i];
+    arr[i] = pivote;
+    return i;
+}
 #endif
