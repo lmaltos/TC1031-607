84a5f44adb074bfc78f80fb6546ca21a82d01265
lmaltos <136147644+lmaltos@users.noreply.github.com>
2024-08-23 13:36:42 +0000

    Agrega ejemplos de qucksort con dobule y char

--- Sorting/main.cpp
+++ Sorting/main.cpp
@@ -1,34 +1,59 @@
 #include <iostream>
 #include "quickSort.h"
 void insertionSort(int arr[], int n);
+template <typename S>
+void print(S[],int);
 
 using namespace std;
 int main() {
+    char char_arr[10] = {'q','w','e','r','t','y','a','s','d','f'};
     int my_arreglo_int[8] = {15,22,13,27,12,10,20,25};
     string my_arreglo[8] = {"hola", "mundo", "quick", "sort", "merge", "bubble", "intercambio", "insercion"};
-    cout << "my_arreglo_int [ ";
-    for (int i = 0; i < 8; i++) {
-        cout << my_arreglo_int[i] << " ";
-    }
+    double arreglo_Double[50];
+
+    for (int i = 0; i < 50; i++)
+        arreglo_Double[i] = (rand() % 100) / double(1.5 + rand() % 5);
+    cout << "arreglo_Double [ ";
+    print(arreglo_Double,20);
     cout << "]" << endl;
+
     cout << "my_arreglo [ ";
-    for (int i = 0; i < 8; i++) {
-        cout << my_arreglo[i] << " ";
-    }
+    print(my_arreglo,8);
     cout << "]" << endl;
+
+    cout << "char_arr [ ";
+    print(char_arr,10);
+    cout << "]" << endl;
+
+    cout << "Llamada a quickSort" << endl;
     //insertionSort(my_arreglo,10);
-    quickSort<int>(my_arreglo_int,0,8);
-    quickSort<string>(my_arreglo,0,8);
+    quickSort(arreglo_Double,0,50);
+    quickSort(my_arreglo,0,8);
+    quickSort(my_arreglo_int,0,8);
+    quickSort(char_arr,0,10);
+
+    cout << "arreglo_Double [ ";
+    print(arreglo_Double,20);
+    cout << "]" << endl;
+
     cout << "my_arreglo_int [ ";
-    for (int i = 0; i < 8; i++) {
-        cout << my_arreglo_int[i] << " ";
-    }
+    print(my_arreglo_int,8);
     cout << "]" << endl;
+
     cout << "my_arreglo [ ";
-    for (int i = 0; i < 8; i++) {
-        cout << my_arreglo[i] << " ";
-    }
+    print(arreglo_Double,20);
     cout << "]" << endl;
+
+    cout << "char_arr [ ";
+    print(char_arr,10);
+    cout << "]" << endl;
+}
+
+template <typename T>
+void print(T arr[],int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
 }
 
 void insertionSort(int arr[], int n) {
